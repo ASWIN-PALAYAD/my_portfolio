a243b8d1d8e224519b225100e23aa5d2633a8ef9
ASWIN-PALAYAD <aswinpalayad07@gmail.com>
2023-12-06 15:42:14 +0530

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section id="Homepage">
+      <Navbar/>
+    </section>
+    <section id="Services">Parallax</section>
+    <section>services</section>
+    <section id="Portfolio">parallax</section>
+    <section>portfolio1</section>
+    <section>portfolio2</section>
+    <section >portfolio3</section>
+    <section id="Contact">Contact</section>
+  </div>;
 };
 
 export default App;
